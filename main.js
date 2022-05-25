2e3377a8d057b03676f31ac8d4d9aa3726bf10ed
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-05-25 14:10:09 -0600

    major changes 05/25

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const daysLeft = document.getElementById('daysLeft');
 const hoursLeft = document.getElementById('hoursLeft');
 const minLeft = document.getElementById('minLeft');
 const secLeft = document.getElementById('secLeft');
-const x = setInterval(function() {
-    const todaysDate = new Date().getTime(); // Todays Date
-    let countDown = countDownDate - todaysDate;
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
-    daysLeft.innerHTML = days;
-    hoursLeft.innerHTML = hours;
-    minLeft.innerHTML = minutes;
-    secLeft.innerHTML = seconds;
+const x = setInterval(function () {
+  const todaysDate = new Date().getTime(); // Todays Date
+  let countDown = countDownDate - todaysDate;
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+  daysLeft.innerHTML = days;
+  hoursLeft.innerHTML = hours;
+  minLeft.innerHTML = minutes;
+  secLeft.innerHTML = seconds;
 }, 1000);
 
 const dropDown = document.getElementById('dropDown');
@@ -27,7 +27,7 @@ function toggleArrow() {
   selectArrow.classList.toggle('select__dropdown-arrow--rotate');
 }
 
-dropDown.addEventListener('click', function() {
+dropDown.addEventListener('click', () => {
   packageModal.classList.toggle('package-modal--expanded');
   toggleArrow();
 })
@@ -45,3 +45,25 @@ for (const elm of selected) {
     toggleArrow();
   })
 }
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const regX = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
+
+email.addEventListener('keyup', (e) => {
+  if (email.value.match(regX)) {
+    email.classList.remove('email-err');
+    email.classList.add('email-success');
+  } else {
+    email.classList.add('email-err');
+    email.placeholder = 'larry@gmail.com';
+    e.preventDefault()
+  }
+})
+
+form.addEventListener('submit', (e) => {
+  if(!email.value.match(regX)) {
+    e.preventDefault();
+  }
+})
+
